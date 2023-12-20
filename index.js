0d7862eea5a46a30eb31c9bf9205f30e428c3164
mohamed helmy <petxtv@gmail.com>
2023-12-21 01:54:42 +0200

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+let admin = require("firebase-admin");
+
+let serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+let hardwareRef = db.collection("hardware");
+
+//send data
+app.post("/api/update", (req, res) => {
+    const data = { value: req.body.value };
+    console.log(data);
+    hardwareRef.doc("reading").set(data);
+    // Your code to save the new user to the database or any other source
+    // Send a response indicating success or failure
+    res.json({ message: "Value updated successfully" });
+});
+
+module.exports = app;
